1ad51a345ad90575d31aa86f6c9d7401d69c37ed
CosmicAxolotl <99707502+CosmicAxolotl@users.noreply.github.com>
2026-02-11 20:50:24 -0800

    Add definition of STRING type & STR_LOG enum

--- src/string_struct.h
+++ src/string_struct.h
@@ -3,5 +3,26 @@
 #ifndef C_BLOCK_COMPILER_STRING_STRUCT_H
 #define C_BLOCK_COMPILER_STRING_STRUCT_H
 
+#include <stddef.h>
+
+typedef struct {
+    char *str;
+    size_t length;
+    size_t buffer;
+} STRING;
+
+typedef enum {
+    /// Good to continue.
+    OK,
+
+    /// Error, can't continue.
+    ERR,
+
+    /// memory allocation error
+    ERR_MEM,
+
+    /// Strings are not equal.
+    DIFF
+} STR_LOG;
 
 #endif
