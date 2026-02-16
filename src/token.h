51f938f57db49889b977b6b8f41643218d492620
CosmicAxolotl <99707502+CosmicAxolotl@users.noreply.github.com>
2026-02-15 21:36:10 -0800

    Add tokenisation finite state machine prototypes

--- src/token.h
+++ src/token.h
@@ -4,5 +4,18 @@
 
 
 
+#include "token/struct.h"
+#include "token/stream.h"
+#include "token/stream_pointer.h"
+
+
+/**
+ * @brief A finite state machine that takes a file string & returns its token stream.
+ * @param _file_str file string to be tokenised
+ * @returns a token stream that contains all legal tokens.
+ */
+TOKEN_STREAM tokenisator_fsm(STRING* _file_str);
+
+
 
 #endif
