f1fe661958f44f938a82361e80cc19042adbac61
CosmicAxolotl <99707502+CosmicAxolotl@users.noreply.github.com>
2026-02-15 21:43:49 -0800

    Implement tkn_type function

--- src/token/struct/operations.c
+++ src/token/struct/operations.c
@@ -9,3 +9,12 @@ void free_token(TOKEN* _target_tkn) {
     _target_tkn->_type = INVALID_STATE;
     _target_tkn->_fpos = NULL_STR_PTR;
 }
+
+
+
+
+TKN_TYPE tkn_type(const TOKEN* _target_tkn) {
+    return _target_tkn->_type;
+}
+
+
