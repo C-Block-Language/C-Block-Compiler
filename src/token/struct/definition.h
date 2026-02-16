c3a8ecb02509f359fb7727f9cfa12b4d8f51d6bd
CosmicAxolotl <99707502+CosmicAxolotl@users.noreply.github.com>
2026-02-15 21:29:04 -0800

    Add reserved word list

--- src/token/struct/definition.h
+++ src/token/struct/definition.h
@@ -36,6 +36,56 @@ typedef struct {
 
 
 #define EMPTY_TOKEN (TOKEN) {._fpos = NULL, ._str = nullptr, ._type = INVALID_STATE}
+
+
+const static STRING reserved_words[] = {
+    // Reserve all modifier execute sub-commands
+    STATIC_STR("execute"),
+    STATIC_STR("if"),
+    STATIC_STR("align"),
+    STATIC_STR("anchored"),
+    STATIC_STR("as"),
+    STATIC_STR("at"),
+    STATIC_STR("facing"),
+    STATIC_STR("in"),
+    STATIC_STR("on"),
+    STATIC_STR("positioned"),
+    STATIC_STR("rotated"),
+    STATIC_STR("summon"),
+    STATIC_STR("run"),
+
+    // reserve return keyword
+    STATIC_STR("return"),
+
+    // reserve native command keyword
+    STATIC_STR("native"),
+    // return function return types
+    STATIC_STR("void"),
+    STATIC_STR("int"),
+
+    // reserve NBT path sources
+    STATIC_STR("nbt_path"),
+    STATIC_STR("entity"),
+    STATIC_STR("storage"),
+    STATIC_STR("block"),
+
+    // reserve score variable type
+    STATIC_STR("score"),
+
+    // reserve nbt operations
+    STATIC_STR("append"),
+    STATIC_STR("insert"),
+    STATIC_STR("merge"),
+    STATIC_STR("prepend"),
+    STATIC_STR("set"),
+    STATIC_STR("remove")
+};
+
+
+constexpr size_t reserved_words_count = sizeof(reserved_words) / sizeof(STRING);
+
+
+
 #endif
 
 
