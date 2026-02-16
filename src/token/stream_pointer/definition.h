e0657d387d9e82cb2d43c962981fc49a1e6802bd
CosmicAxolotl <99707502+CosmicAxolotl@users.noreply.github.com>
2026-02-15 21:34:40 -0800

    Add token stream pointer initial files

---
+++ src/token/stream_pointer/definition.h
@@ -0,0 +1,20 @@
+
+
+#ifndef C_BLOCK_COMPILER_TOKEN_STREAM_PTR_DEF_H
+#define C_BLOCK_COMPILER_TOKEN_STREAM_PTR_DEF_H
+
+
+
+/**
+ * @brief A fail-safe pointer to iterate in & out of a token stream.
+ */
+typedef struct {
+    TOKEN_STREAM* _tkn_strm;
+    size_t _index;
+} TKN_STREAM_PTR;
+
+#define NULL_TOKEN_STREAM_PTR (TKN_STREAM_PTR) {_tkn_strm = nullptr, _index = 0}
+
+#endif
+
+
---
+++ src/token/stream_pointer/operations.c
@@ -0,0 +1,5 @@
+
+#include "operations.h"
+
+
+
