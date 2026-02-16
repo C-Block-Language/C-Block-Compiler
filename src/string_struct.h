6cef7c49f07204e7e1e468db593e40052bd68aad
CosmicAxolotl <99707502+CosmicAxolotl@users.noreply.github.com>
2026-02-15 21:14:15 -0800

    Refactor string pointer definition

--- src/string_struct.h
+++ src/string_struct.h
@@ -5,21 +5,6 @@
 
 #include <stddef.h>
 
- * @brief A fail-safe pointer to iterate in & out of a string struct.
- */
-typedef struct {
-    STRING* _str_struct;
-    size_t _index;
-    size_t _line;
-    size_t _column;
-} STR_PTR;
-
-
-
-/**
- * @brief An initialiser macro helper for a null string pointer.
- */
-#define NULL_STR_PTR (STR_PTR){._str_struct = nullptr, ._index = 0, ._line = 1, ._column = 1}
 
 
 
