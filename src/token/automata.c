ed6e754b456f1eb5cd75e549ef270fae24e7eab4
CosmicAxolotl <99707502+CosmicAxolotl@users.noreply.github.com>
2026-02-20 15:54:39 -0800

    Add TODO to automata.c

--- src/token/automata.c
+++ src/token/automata.c
@@ -50,6 +50,7 @@ TOKEN_STREAM tokenisator_automata(STRING *_file_str) {
 static TOKEN return_tkn(const TKN_TYPE type, STR_PTR* s_pos, const STR_PTR e_pos) {
     auto tkn = EMPTY_TOKEN;
     auto str = EMPTY_STR;
+    /// TODO: add stop if returns `ERR_MEM`
     (void) str_ptrt_struct(&str, s_pos, &e_pos);
 
     tkn._fpos = *s_pos;
